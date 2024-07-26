33a8c555d6e2ad9b8c75e25f8c92b782ccd4b64f
Alexey Shelby <shelbyhell@proton.me>
2024-07-26 03:34:10 +0000

    topaz: Inherit bt component.
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- device.mk
+++ device.mk
@@ -33,20 +33,13 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
-# A2DP
-PRODUCT_PACKAGES += \
-  liba2dpoffload \
-  android.hardware.bluetooth.a2dp@1.0-impl \
-  android.hardware.bluetooth.a2dp@1.0-service
-
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService-Soong \
-    com.dsi.ant@1.0.vendor
+    AntHalService-Soong
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -56,22 +49,6 @@ PRODUCT_COPY_FILES += \
 # Authsecret
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio-impl \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio-V2-ndk.vendor \
-    audio.bluetooth.default \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.bluetooth.audio-V1-ndk.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -381,11 +358,13 @@ TARGET_KERNEL_VERSION := 5.15
 TARGET_ADRENO_COMPONENT_VARIANT := adreno-s
 TARGET_WLAN_COMPONENT_VARIANT := wlan
 TARGET_USE_AIDL_QTI_HEALTH := true
+TARGET_USE_AIDL_QTI_BT_AUDIO := true
 
 TARGET_COMMON_QTI_COMPONENTS += \
     adreno \
     av \
     audio \
+    bt \
     perf \
     vibrator \
     usb \
