aaeee510f4c1c0ae9515df6d61f1b0b84bbbab35
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-08-04 00:34:12 +0000

    topaz: props: Use lmk kill_timeout_ms value from CLO
    [1] - Qualcomm has its own adaptation, via ro.lmk.kill_timeout_ms_dup. In this case we will just use the same value
    
    Change-Id: I350a0bd2502fb3e42db606d51b513137bc9a8509
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -209,7 +209,7 @@ KERNEL_PREBUILT_DIR := device/xiaomi/topaz-kernel
 # LMK
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.lmk.filecache_min_kb=153600 \
-    ro.lmk.kill_timeout_ms=50 \
+    ro.lmk.kill_timeout_ms=100 \
     ro.lmk.stall_limit_critical=40
 
 # Media
