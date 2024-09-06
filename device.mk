81fb7f09d4ea4d4fb5a175884bb62975b8b776ba
Arian <arian.kulmer@web.de>
2024-09-06 14:01:17 +0200

    topaz: Patch vendor.libdpmframework.so to use libhidlbase_shim
    Change-Id: Iefe50b82b5b13bbdee10819a0c60c14240fab7ce

--- device.mk
+++ device.mk
@@ -114,6 +114,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# DPM
+PRODUCT_PACKAGES += \
+    libhidlbase_shim
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
