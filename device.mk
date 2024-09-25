e48eca90947cc62b65049cb995bb2485d0b4a487
Alexey Shelby <shelbyhell@proton.me>
2024-09-25 15:01:56 +0000

    topaz: Move init script to `init/`.
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- device.mk
+++ device.mk
@@ -195,7 +195,7 @@ PRODUCT_PACKAGES += \
     ueventd.topaz.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # Keymaster
 PRODUCT_PACKAGES += \
