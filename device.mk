78f6efbe4acc68a723c05206dafb100b0fa975ab
Alexey Shelby <shelbyhell@proton.me>
2024-11-19 05:07:46 +0000

    topaz: Use msm_irqbalance from qcom/common.
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- device.mk
+++ device.mk
@@ -322,10 +322,6 @@ TARGET_COMMON_QTI_COMPONENTS += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Perf
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
